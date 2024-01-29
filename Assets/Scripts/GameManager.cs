a6aec791bd7f5c5df077856d298f93ac09b1f227
LeslieTheMagicStone <lesliethemagicstone@gmail.com>
2024-01-30 00:03:12 +0800

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject Bubble;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            var bubble = Instantiate(Bubble);
+            bubble.transform.position = new(Random.Range(-5, 5), Random.Range(3, 4), 0);
+        }
+    }
+}
